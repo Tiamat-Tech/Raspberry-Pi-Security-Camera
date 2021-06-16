960f338bf451ded4a8521f41d64305e2ba746d24
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-16 10:10:48 +0200

    mail_config

--- mail_config.py
+++ mail_config.py
@@ -13,7 +13,7 @@
 #     msg_alternative = MIMEMultipart('alternative')
 #     msg_root.attach(msg_alternative)
 #     msg_text = MIMEText('Smart security cam found object')
-#     msg_alternative.attach(msg_text)
+    msg_alternative.attach(msg_text)
 
     msg_text = MIMEText('<img src="cid:image1">', 'html')
     msg_alternative.attach(msg_text)
