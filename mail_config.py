8657a88691e53748890feb1f6df682d7fddf8f28
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-16 10:10:02 +0200

    mail_config

--- mail_config.py
+++ mail_config.py
@@ -26,4 +26,4 @@
 #     smtp.starttls()
 #     smtp.login(config.sender_email_address, config.sender_email_password)
 #     smtp.sendmail(config.sender_email_address, config.receiver_email_address, msg_root.as_string())
-#     smtp.quit()
+    smtp.quit()
