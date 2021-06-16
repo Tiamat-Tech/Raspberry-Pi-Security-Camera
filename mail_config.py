91110f448cdf0e2132875b1ada1211e3f0eabcb7
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-16 10:16:33 +0200

    mail_config

--- mail_config.py
+++ mail_config.py
@@ -1,7 +1,7 @@
 # import smtplib
 # from email.mime.multipart import MIMEMultipart
 # from email.mime.text import MIMEText
-# from email.mime.image import MIMEImage
+from email.mime.image import MIMEImage
 
 def send_email(image, config):
     msg_root = MIMEMultipart('related')
